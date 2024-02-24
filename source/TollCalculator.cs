e31c5719a4baa962120d810e01c27c3619222cf8
OmerSahinKeskin <omersahin.keskin97@gmail.com>
2024-02-24 11:18:48 +0100

    Moved enum to own file and changed isTollFreeVehicle to reflect changes made

--- source/TollCalculator.cs
+++ source/TollCalculator.cs
@@ -23,7 +23,7 @@ public class TollCalculator
             int tempFee = GetTollFee(intervalStart, vehicle);
 
             long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-            long minutes = diffInMillies/1000/60;
+            long minutes = diffInMillies / 1000 / 60;
 
             if (minutes <= 60)
             {
@@ -42,14 +42,12 @@ public class TollCalculator
 
     private bool IsTollFreeVehicle(Vehicle vehicle)
     {
+
         if (vehicle == null) return false;
         String vehicleType = vehicle.GetVehicleType();
-        return vehicleType.Equals(TollFreeVehicles.Motorbike.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Tractor.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Emergency.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Diplomat.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Foreign.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Military.ToString());
+        List<string> tollFreeVehicles = Enum.GetNames(typeof(TollFreeVehicles)).ToList();
+
+        return tollFreeVehicles.Contains(vehicleType);
     }
 
     public int GetTollFee(DateTime date, Vehicle vehicle)
@@ -94,15 +92,5 @@ public class TollCalculator
             }
         }
         return false;
-    }
-
-    private enum TollFreeVehicles
-    {
-        Motorbike = 0,
-        Tractor = 1,
-        Emergency = 2,
-        Diplomat = 3,
-        Foreign = 4,
-        Military = 5
     }
 }
